df59232a51d4e9a5ce7ffa0ad29ec8015546b166
Joshua Draxten <joshuadraxten@live.com>
2019-06-17 16:49:02 -0500

    Update version tags

--- wpmktgengine.php
+++ wpmktgengine.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 3.7.13
+    Version: 3.7.14
     License: GPLv2
     Text Domain: wpmktgengine
 */
