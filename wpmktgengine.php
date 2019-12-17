8dc8a93c101f7587a5aa23d334a38fb923368149
Martin Picha <martin@latorante.name>
2019-12-17 20:33:38 +1100

    Update version

--- wpmktgengine.php
+++ wpmktgengine.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 3.8.5
+    Version: 3.8.6
     License: GPLv2
     Text Domain: wpmktgengine
 */
