299c5e52e2e6c9f941811125752b99516ef6507d
Martin Picha <martin@latorante.name>
2021-05-06 07:49:45 +1000

    4.0.2

--- wpmktgengine.php
+++ wpmktgengine.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 4.0.1
+    Version: 4.0.2
     License: GPLv2
     Text Domain: wpmktgengine
 */
