d88061128849ba591bf134ef2372d6a0f76111b4
Martin Picha <martin@latorante.name>
2021-06-17 09:36:13 +1000

    Changes to the script

--- deploy/increment.sh
+++ deploy/increment.sh
@@ -5,7 +5,6 @@ PLUGIN_CURRENT_VERSION=$(awk '/   Version/{print $NF}' wpmktgengine.php)
 PLUGIN_NEXT_VERSION=$(echo $PLUGIN_CURRENT_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
 
 read -p "Would you like to update plugin from $PLUGIN_CURRENT_VERSION to $PLUGIN_NEXT_VERSION ?" response
-response=${response,,} # tolower
 if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
   echo "Updating to a new version..."
   # New version update
@@ -15,6 +14,7 @@ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
   # - In readme file
   sed -i "" "s/Stable tag: ${PLUGIN_CURRENT_VERSION}/Stable tag: ${PLUGIN_NEXT_VERSION}/g" readme.txt
   # New tag and push
+  git commit -am "Release: $PLUGIN_NEXT_VERSION"
   git tag -a $PLUGIN_NEXT_VERSION -m "Release: $PLUGIN_NEXT_VERSION"
   git push origin master --tags
   # All done, yay
