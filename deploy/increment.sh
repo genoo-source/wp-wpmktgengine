a6a1148cb6d0484466e08b5a969c5510bdacc478
Martin Picha <martin@latorante.name>
2021-06-17 08:52:32 +1000

    Add Github Action Script and chagne increment script

--- deploy/increment.sh
+++ deploy/increment.sh
@@ -4,10 +4,19 @@
 PLUGIN_CURRENT_VERSION=$(awk '/   Version/{print $NF}' wpmktgengine.php)
 PLUGIN_NEXT_VERSION=$(echo $PLUGIN_CURRENT_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
 
-# Replace versions with new version
-# - In main file
-sed -i "" "s/${PLUGIN_CURRENT_VERSION}/${PLUGIN_NEXT_VERSION}/g" wpmktgengine.php
-# - In readme file
-sed -i "" "s/Stable tag: ${PLUGIN_CURRENT_VERSION}/Stable tag: ${PLUGIN_NEXT_VERSION}/g" readme.txt
-
-echo "New version: "$PLUGIN_NEXT_VERSION
+read -p "Would you like to update plugin from $PLUGIN_CURRENT_VERSION to $PLUGIN_NEXT_VERSION ?" response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+  echo "Updating to a new version..."
+  # New version update
+  # Replace versions with new version
+  # - In main file
+  sed -i "" "s/${PLUGIN_CURRENT_VERSION}/${PLUGIN_NEXT_VERSION}/g" wpmktgengine.php
+  # - In readme file
+  sed -i "" "s/Stable tag: ${PLUGIN_CURRENT_VERSION}/Stable tag: ${PLUGIN_NEXT_VERSION}/g" readme.txt
+  # New tag and push
+  git tag -a $PLUGIN_NEXT_VERSION -m "Release: $PLUGIN_NEXT_VERSION"
+  git push origin master --tags
+  # All done, yay
+  echo "Updated new version"
+fi
