a98558ff88640bb629715e5cf814664818261b05
GenooBot <admin@genoo.com>
2022-07-26 14:10:03 +0000

    GenooBot: Release 2022-07-26T14:10:03+00:00

--- wpmktgengine.php
+++ wpmktgengine.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 4.0.16
+    Version: 4.0.17
     License: GPLv2
     Text Domain: wpmktgengine
 */
